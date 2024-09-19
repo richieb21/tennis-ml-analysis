140ae7d003b5450de5cb93fe81a7d1293ec61a23
richieb21 <76607899+richieb21@users.noreply.github.com>
2024-09-19 18:30:42 -0400

    Export Video Utils to Read and Save Files

---
+++ utils/video_utils.py
@@ -0,0 +1,28 @@
+import cv2
+
+# Read a video file and return a list of frames
+def read_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+
+    # Read the video frame by frame, breaking when there is an unsuccessful read
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+
+    cap.release()
+    return frames
+
+# Save a video from a list of frames
+def save_video(output_video_frames, output_video_path):
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+
+    for frame in output_video_frames:
+        out.write(frame)
+
+    out.release()
+    print(f"Video saved to {output_video_path}")
+
